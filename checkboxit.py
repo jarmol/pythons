3e1436919ab41e4d900c091b445671a27961f3c9
jarmol <jarmol@users.noreply.github.com>
2014-09-06 19:32:48 +0300

    Checkbuttons example

--- checkboxit.py
+++ checkboxit.py
@@ -1,6 +1,6 @@
+import Tkinter
 from Tkinter import *
 import tkMessageBox
-import Tkinter
 
 top = Tkinter.Tk()
 top.title('Raksi ruutuun')
@@ -9,17 +9,23 @@ top.geometry("200x240+100+100")
 CheckVar1 = IntVar()
 CheckVar2 = IntVar()
 CheckVar3 = IntVar()
+CheckVar4 = IntVar()
 
 C1 = Checkbutton(top, text = "Musiikkia", variable = CheckVar1, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 20)
-C2 = Checkbutton(top, text = "Videoita", variable = CheckVar2, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 20)
-C3 = Checkbutton(top, text = "Facebook", variable = CheckVar3, \
-                 onvalue = 1, offvalue = 0, height=5, \
-                 width = 20)
+                 onvalue = 1, offvalue = 0, height=2, \
+                 width = 16, activebackground = 'lightyellow')
+C2 = Checkbutton(top, text = "Videoita ", variable = CheckVar2, \
+                 onvalue = 1, offvalue = 0, height=2, \
+                 width = 16, activebackground = 'lightgreen')
+C3 = Checkbutton(top, text = "Facebook ", variable = CheckVar3, \
+                 onvalue = 1, offvalue = 0, height=2, \
+                 width = 16, activebackground = 'lightblue')
+C4 = Checkbutton(top, text = "Grillimakkaraa", variable = CheckVar4, \
+                 onvalue = 1, offvalue = 0, height=2, \
+                 width = 16, activebackground = 'pink')
+
 C1.pack()
 C2.pack()
 C3.pack()
+C4.pack()
 top.mainloop()
