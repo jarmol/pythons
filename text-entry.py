518db63240acb9aaf887a080be12426d25dbac81
jarmol <jarmol@users.noreply.github.com>
2014-09-09 20:04:09 +0300

    Get and show the read entry.

--- text-entry.py
+++ text-entry.py
@@ -1,11 +1,23 @@
 from Tkinter import *
+import tkMessageBox
+
+def callback():
+    s = E1.get()
+    tkMessageBox.showinfo( "User", s)
 
 top = Tk()
+top.geometry('250x100+40+40')
+top.title('Text entry field')
+b = Button(top, text="get", command=callback)
+b.pack(side = TOP)
+
 L1 = Label(top, text="User Name")
 L1.pack( side = LEFT)
 E1 = Entry(top, bd =5)
-
 E1.pack(side = RIGHT)
+E1.insert(0, "User name")
+#b = Button(top, text="get", command=callback)
+#b.pack(side = TOP)
 
 top.mainloop()
 # http://www.tutorialspoint.com/python/tk_entry.htm
