f4cb514d5c6ce4ed9286575327c302725105417f
jarmol <jarmol@users.noreply.github.com>
2014-09-13 21:11:08 +0300

    GIF image and Frame as separator

---
+++ gifimage.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import Tkinter as tk
+
+master = tk.Tk()
+#master.geometry("250x150+50+50")
+master.title("Frame-erotin")
+
+tk.Label(text="Yläosa", fg="red", pady=15).pack()
+
+separator = tk.Frame(height=2, bd=1, relief=tk.SUNKEN)
+separator.pack(fill=tk.X, padx=5, pady=5)
+
+tk.Label(text="Alaosa", fg="blue").pack()
+photo = tk.PhotoImage(file="icon.gif")
+tk.Label(image=photo).pack()
+tk.Label().pack()
+tk.mainloop()
