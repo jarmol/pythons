e151d44b25d8a0b0ee8c2d48f96107a1a542a68f
Polarit <jarmol@users.noreply.github.com>
2026-02-04 09:56:26 +0200

    The module imported to the main

--- mypython/solfuns.py
+++ mypython/solfuns.py
@@ -5,6 +5,9 @@ from math import pi, sin, asin, cos, tan, acos
 def rad(x):
     return(pi * x / 180.0)
 
+def deg(x):
+    return(180.0 * x / pi)
+
 def jdn_from_date(yr, mnt, day) :
     result = 367*yr - 7*(yr + (mnt + 9)//12)//4 \
     - 3*((yr + (mnt - 9)//7)//100 + 1)//4 \
@@ -52,6 +55,8 @@ def sun_app_long(jc, stl):
     sal = stl - 0.00569 - 0.00478 * sin(rad(omega))
     return sal
 
+# sal = sun_app_long(jc, stl)
+
 def mean_obliq_ecliptic(jc):
     """Calculate the Mean Obliquity of the Ecliptic (in degrees)"""
     moe = 23.0 + (26.0 + ((21.448 - jc * (46.815 \
@@ -63,3 +68,86 @@ def obliq_corr(jc, moe):
     omega = 125.04 - 1934.136 * jc
     oc = moe + 0.00256 * cos(rad(omega))
     return oc
+
+# oc = obliq_corr(jc, moe)
+
+def sun_declination(oc, sal):
+# Calculate the Sun's Declination (in degrees)
+    sd = deg(asin(sin(rad(oc))*sin(rad(sal))))
+    return sd
+
+# sd = sun_declination(oc, sal)
+
+def y_variable(jc, oc):
+    """Calculate the Y variable for the equation of time"""
+    y = tan(rad(oc) / 2.0) * tan(rad(oc) / 2.0)
+    return y
+
+"""Equation of Time (in minutes)"""
+def equation_of_time(y, gmls, eoe, gmas):
+    a = rad(gmls)
+    b = rad(gmas)
+    eot = y * sin(2 * a) - 2 * eoe * sin(b) \
+    + 4 * eoe * y * sin(b) * cos(2 * a) \
+    - 0.5 * y * y * sin(4 * a) - 1.25 * eoe * eoe * sin(2 * b)
+    eot = deg(eot) * 4.0  # Convert to minutes
+    return eot
+
+def ha_sunrise(jc, lat, sd):
+    """Calculate the Hour Angle at Sunrise (in degrees)"""
+    ha = deg(acos(cos(rad(90.833)) / (cos(rad(lat)) * cos(rad(sd))) - tan(rad(lat)) * tan(rad(sd))))
+    return ha
+
+def haSunrise(lat, sd):
+    haS = deg(acos(cos(rad(90.833)) / (cos(rad(lat)) * cos(rad(sd))) - tan(rad(lat)) * tan(rad(sd))))
+    return haS
+
+def true_solar_time(longitude, hr, mn, sc, tz_offset, eot=0.0):
+    # Calculate the True Solar Time (in minutes)
+    # time_in_minutes: local time in minutes (0..1440)
+    time_in_minutes = hr * 60 + mn + sc / 60
+    # tz_offset from time.timezone is seconds west of UTC -> hours west of UTC
+    # spreadsheet/timezone convention: positive for east of Greenwich
+    tz = -tz_offset
+    # Apply equation of time (minutes) and longitude/timezone offsets
+    tst = (time_in_minutes + eot + 4 * longitude - 60 * tz) % 1440
+    return tst
+
+def solar_noon(longitude, eot, tzoffs):
+    # =(720-4*$B$4-V2+$B$5*60)/1440
+    day_fraction = (720 - 4 * longitude - eot - tzoffs * 60) / 1440
+    noon_as_minutes = 720 - 4 * longitude - eot - tzoffs * 60
+    noon_hours = int(noon_as_minutes) // 60
+    noon_minutes = int(noon_as_minutes) % 60
+    noon_seconds = (60*noon_as_minutes) % 60
+    noon_hr_mn_str = (f"Noon time {noon_hours}:{noon_minutes}:{round(noon_seconds, 2)}")
+    return [noon_hr_mn_str, day_fraction]
+
+def hour_angle(tcurrent, jc, lon):
+    """Calculate the Hour Angle (in degrees)"""
+    ha = (tcurrent / 4.0) - 180.0 # + lon
+    if ha < -180.0:
+        ha += 360.0
+    return ha
+
+def solar_zenith_angle(tcurrent, ha, lat, sd):
+    """Calculate the Solar Zenith Angle (in degrees)"""
+    sins = sin(rad(lat)) * sin(rad(sd))
+    coss = cos(rad(lat)) * cos(rad(sd)) * cos(rad(ha))
+    sza = deg(acos(sins + coss))
+    return sza
+
+
+def solar_azimuth(ha, sza, sd, lat):
+    """Calculate the Solar Azimuth Angle (in degrees clockwise from north)"""
+    saz = 0.0
+    sin_az = (cos(rad(sd)) * sin(rad(ha))) / sin(rad(sza))
+    cos_az = ( (sin(rad(sd)) - sin(rad(lat)) * cos(rad(sza))) /
+               (cos(rad(lat)) * sin(rad(sza))) )
+    az_rad = acos(cos_az)
+    az_deg = deg(az_rad)
+    if sin_az < 0:
+        saz = az_deg
+    else:
+        saz = 360.0 - az_deg
+    return saz
